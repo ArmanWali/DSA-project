095aa32bd7cb12853fab0d8a71ea8b674300c705
Abdullah-KhanJr <63127711+Abdullah-KhanJr@users.noreply.github.com>
2024-12-20 20:33:42 +0500

    Emergency 4

--- Emergency_Services.cpp
+++ Emergency_Services.cpp
@@ -4,140 +4,33 @@
 #include <unordered_map>
 using namespace std;
 
+// Function prototypes
+class Stack;
+class Graph;
+class CrowdControl;
+
 class Stack {
     vector<string> stack;
 
 public:
-    void push(string name) {
-        stack.push_back(name);
-    }
-
-    void pop() {
-        if (!stack.empty()) {
-            stack.pop_back();
-        }
-    }
-
-    string top() {
-        if (!stack.empty()) {
-            return stack.back();
-        }
-        return "";
-    }
-
-    bool empty() {
-        return stack.empty();
-    }
+    void push(string name);
+    void pop();
+    string top();
+    bool empty();
 };
 
 class Graph {
     unordered_map<string, vector<pair<string, int>>> adjList;
 
 public:
-    void addEdge(string u, string v, int weight) {
-        adjList[u].push_back({v, weight});
-        adjList[v].push_back({u, weight});
-    }
+    void addEdge(string u, string v, int weight);
+    void displayGraph();
+    void bfs(string start);
+    void dfs(string start);
+    void ambulanceRouteOptimization(string start, string end);
 
-    void displayGraph() {
-        cout << "Graph Representation:" << endl;
-        for (auto& node : adjList) {
-            cout << node.first << " -> ";
-            for (auto& neighbor : node.second) {
-                cout << "(" << neighbor.first << ", " << neighbor.second << ") ";
-            }
-            cout << endl;
-        }
-    }
-
-    void bfs(string start) {
-        queue<string> q;
-        unordered_map<string, bool> visited;
-
-        q.push(start);
-        visited[start] = true;
-
-        cout << "BFS Traversal starting from " << start << ": ";
-        while (!q.empty()) {
-            string node = q.front();
-            q.pop();
-            cout << node << " ";
-
-            for (auto& neighbor : adjList[node]) {
-                if (!visited[neighbor.first]) {
-                    visited[neighbor.first] = true;
-                    q.push(neighbor.first);
-                }
-            }
-        }
-        cout << endl;
-    }
-
-    void dfsUtil(string node, unordered_map<string, bool>& visited) {
-        cout << node << " ";
-        visited[node] = true;
-
-        for (auto& neighbor : adjList[node]) {
-            if (!visited[neighbor.first]) {
-                dfsUtil(neighbor.first, visited);
-            }
-        }
-    }
-
-    void dfs(string start) {
-        unordered_map<string, bool> visited;
-        cout << "DFS Traversal starting from " << start << ": ";
-        dfsUtil(start, visited);
-        cout << endl;
-    }
-
-    void ambulanceRouteOptimization(string start, string end) {
-        unordered_map<string, int> distance;
-        unordered_map<string, string> parent;
-        for (auto& node : adjList) {
-            distance[node.first] = INT_MAX;
-        }
-        distance[start] = 0;
-
-        priority_queue<pair<int, string>, vector<pair<int, string>>, greater<pair<int, string>>> pq;
-        pq.push({0, start});
-
-        while (!pq.empty()) {
-            string current = pq.top().second;
-            int currentDist = pq.top().first;
-            pq.pop();
-
-            if (current == end) break;
-
-            for (auto& neighbor : adjList[current]) {
-                int newDist = currentDist + neighbor.second;
-                if (newDist < distance[neighbor.first]) {
-                    distance[neighbor.first] = newDist;
-                    pq.push({newDist, neighbor.first});
-                    parent[neighbor.first] = current;
-                }
-            }
-        }
-
-        if (distance[end] == INT_MAX) {
-            cout << "No route found from " << start << " to " << end << endl;
-        } else {
-            cout << "Optimized Route (Ambulance): ";
-            string node = end;
-            Stack path;
-            while (node != start) {
-                path.push(node);
-                node = parent[node];
-            }
-            path.push(start);
-            while (!path.empty()) {
-                cout << path.top();
-                path.pop();
-                if (!path.empty()) cout << " -> ";
-            }
-            cout << " | Distance: " << distance[end] << endl;
-        }
-    }
+private:
+    void dfsUtil(string node, unordered_map<string, bool>& visited);
 };
 
 class CrowdControl {
@@ -145,34 +38,168 @@ class CrowdControl {
     queue<string> crowdQueue;
 
 public:
-    void addPersonToStack(string name) {
-        crowdStack.push(name);
-        cout << name << " added to Stack." << endl;
-    }
-
-    void removePersonFromStack() {
-        if (!crowdStack.empty()) {
-            cout << crowdStack.top() << " removed from Stack." << endl;
-            crowdStack.pop();
-        } else {
-            cout << "No one in the Stack." << endl;
-        }
-    }
-
-    void addPersonToQueue(string name) {
-        crowdQueue.push(name);
-        cout << name << " added to Queue." << endl;
-    }
-
-    void removePersonFromQueue() {
-        if (!crowdQueue.empty()) {
-            cout << crowdQueue.front() << " removed from Queue." << endl;
-            crowdQueue.pop();
-        } else {
-            cout << "No one in the Queue." << endl;
-        }
-    }
+    void addPersonToStack(string name);
+    void removePersonFromStack();
+    void addPersonToQueue(string name);
+    void removePersonFromQueue();
 };
+
+// Stack methods
+void Stack::push(string name) {
+    stack.push_back(name);
+}
+
+void Stack::pop() {
+    if (!stack.empty()) {
+        stack.pop_back();
+    }
+}
+
+string Stack::top() {
+    if (!stack.empty()) {
+        return stack.back();
+    }
+    return "";
+}
+
+bool Stack::empty() {
+    return stack.empty();
+}
+
+// Graph methods
+void Graph::addEdge(string u, string v, int weight) {
+    adjList[u].push_back({v, weight});
+    adjList[v].push_back({u, weight});
+}
+
+void Graph::displayGraph() {
+    cout << "Graph Representation:" << endl;
+    for (auto& node : adjList) {
+        cout << node.first << " -> ";
+        for (auto& neighbor : node.second) {
+            cout << "(" << neighbor.first << ", " << neighbor.second << ") ";
+        }
+        cout << endl;
+    }
+}
+
+void Graph::bfs(string start) {
+    queue<string> q;
+    unordered_map<string, bool> visited;
+
+    q.push(start);
+    visited[start] = true;
+
+    cout << "BFS Traversal starting from " << start << ": ";
+    while (!q.empty()) {
+        string node = q.front();
+        q.pop();
+        cout << node << " ";
+
+        for (auto& neighbor : adjList[node]) {
+            if (!visited[neighbor.first]) {
+                visited[neighbor.first] = true;
+                q.push(neighbor.first);
+            }
+        }
+    }
+    cout << endl;
+}
+
+void Graph::dfsUtil(string node, unordered_map<string, bool>& visited) {
+    cout << node << " ";
+    visited[node] = true;
+
+    for (auto& neighbor : adjList[node]) {
+        if (!visited[neighbor.first]) {
+            dfsUtil(neighbor.first, visited);
+        }
+    }
+}
+
+void Graph::dfs(string start) {
+    unordered_map<string, bool> visited;
+    cout << "DFS Traversal starting from " << start << ": ";
+    dfsUtil(start, visited);
+    cout << endl;
+}
+
+void Graph::ambulanceRouteOptimization(string start, string end) {
+    unordered_map<string, int> distance;
+    unordered_map<string, string> parent;
+    for (auto& node : adjList) {
+        distance[node.first] = INT_MAX;
+    }
+    distance[start] = 0;
+
+    priority_queue<pair<int, string>, vector<pair<int, string>>, greater<pair<int, string>>> pq;
+    pq.push({0, start});
+
+    while (!pq.empty()) {
+        string current = pq.top().second;
+        int currentDist = pq.top().first;
+        pq.pop();
+
+        if (current == end) break;
+
+        for (auto& neighbor : adjList[current]) {
+            int newDist = currentDist + neighbor.second;
+            if (newDist < distance[neighbor.first]) {
+                distance[neighbor.first] = newDist;
+                pq.push({newDist, neighbor.first});
+                parent[neighbor.first] = current;
+            }
+        }
+    }
+
+    if (distance[end] == INT_MAX) {
+        cout << "No route found from " << start << " to " << end << endl;
+    } else {
+        cout << "Optimized Route (Ambulance): ";
+        string node = end;
+        Stack path;
+        while (node != start) {
+            path.push(node);
+            node = parent[node];
+        }
+        path.push(start);
+        while (!path.empty()) {
+            cout << path.top();
+            path.pop();
+            if (!path.empty()) cout << " -> ";
+        }
+        cout << " | Distance: " << distance[end] << endl;
+    }
+}
+
+// CrowdControl methods
+void CrowdControl::addPersonToStack(string name) {
+    crowdStack.push(name);
+    cout << name << " added to Stack." << endl;
+}
+
+void CrowdControl::removePersonFromStack() {
+    if (!crowdStack.empty()) {
+        cout << crowdStack.top() << " removed from Stack." << endl;
+        crowdStack.pop();
+    } else {
+        cout << "No one in the Stack." << endl;
+    }
+}
+
+void CrowdControl::addPersonToQueue(string name) {
+    crowdQueue.push(name);
+    cout << name << " added to Queue." << endl;
+}
+
+void CrowdControl::removePersonFromQueue() {
+    if (!crowdQueue.empty()) {
+        cout << crowdQueue.front() << " removed from Queue." << endl;
+        crowdQueue.pop();
+    } else {
+        cout << "No one in the Queue." << endl;
+    }
+}
 
 int main() {
     Graph emergencyGraph;
